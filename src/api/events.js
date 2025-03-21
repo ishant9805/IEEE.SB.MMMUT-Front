c8b9f23a76daf1e726467dad7514b0b4b5e36c6f
Ieeesbmmmut01 <ieee.stb.mmmut@gmail.com>
2025-03-21 23:41:59 +0530

    Update events.js

--- src/api/events.js
+++ src/api/events.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // ✅ Base URL for events API
 // const API_URL = 'https://ieee-back.vercel.app/api/events';
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://ieee-back.vercel.app/api';
 
 // ✅ Axios instance with global config
 const axiosInstance = axios.create({
