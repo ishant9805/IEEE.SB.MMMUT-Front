7a448a5d26f3f8f1503ce7c5b99cdf88e7055499
Ieeesbmmmut01 <ieee.stb.mmmut@gmail.com>
2026-08-13 23:07:36 +0530

    Update AdminContactSubmissions.jsx

--- src/pages/AdminContactSubmissions.jsx
+++ src/pages/AdminContactSubmissions.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import { getContactSubmissions, deleteContactSubmission } from '../api/contact';
 import ContactSubmissionForm from '../components/ContactSubmissionForm';
 
