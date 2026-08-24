3beda28005ac633656f02db9eba26d4748f32848
Ieeesbmmmut01 <ieee.stb.mmmut@gmail.com>
2026-08-24 12:03:01 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
         {/* Logo */}
         <div className="flex items-center space-x-3">
           <Link to="/" className="flex items-center">
-            <img src="/logo.png" alt="IEEE-SB-MMMUT" className="h-10" />
+            <img src="/logo.webp" alt="IEEE-SB-MMMUT" className="h-10" />
             <span className="text-xl font-extrabold tracking-wide">IEEE STB MMMUT</span>
           </Link>
         </div>
